fa82eec83a00d7d67e8597ecd7a7455e63c09a2c
InspectorSolaris <german.dark00@gmail.com>
2019-02-10 17:44:31 +0700

    Title screen and 3 question-screen without any activities, except title

--- app/src/main/java/com/example/project2/MainActivity.kt
+++ app/src/main/java/com/example/project2/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.project2
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonBegin(view: View){
+        val nextIntent = Intent(this, Question1Activity::class.java)
+        startActivity(nextIntent)
+    }
+
+    fun buttonExit(view: View){
+        finish()
+        moveTaskToBack(true)
     }
 }
